277375c2aac8935c99a673a1a738fed5b8654db4
Name <mail>
2023-02-15 19:55:38 +0500

    site retrival

---
+++ js/ASIS.js
@@ -0,0 +1,89 @@
+$("html").niceScroll({
+    mousescrollstep: 70,
+    cursorcolor: "#eb7315",
+    zindex: 12,
+    cursorwidth: "5px",
+    cursorborderradius: "10px",
+    cursorborder: "none",
+});
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("TelephoneSend2");
+var inptvalue = document.getElementById("TelephoneValue");
+var inptuser = document.getElementById("TelephoneUser");
+var inptmsg = document.getElementById("TelephoneMessage");
+var span = document.getElementsByClassName("close")[0];
+var myModal2 = document.getElementById("myModal2");
+var request;
+
+  $("html").bind('change keyup input click', function () {
+      if (inptvalue.value.match(/[^0-9]/g)) {
+          inptvalue.value = inptvalue.value.replace(/[^0-9]/g, '');
+      }
+  });
+
+  $("#TelephoneSend").submit(function(event){
+    event.preventDefault();
+    if (request) {
+      request.abort();
+    }
+    var $form = $(this);
+    var $inputs = $form.find("input, select, button, textarea");
+    var serializedData = $form.serialize();
+    $inputs.prop("disabled", true);
+    request = $.ajax({
+      url: "telephone.php",
+      type: "post",
+      data: serializedData
+    });
+    request.always(function () {
+      $inputs.prop("disabled", false);
+      $(btn).prop('disabled', false);
+    });
+    request.done(function (jqXHR){
+      if (inptvalue.value.length >10){
+        if (inptmsg.value.length >0){
+          myModal2.innerHTML="Отправка выполнена. Спасибо, с вами свяжутся.";
+          $(btn).prop('disabled', true);
+          modal.style.display = "block";
+          setTimeout(function() {
+              inptvalue.value='';
+              inptuser.value='';
+              inptmsg.value='';
+              modal.style.display = "none";
+          }.bind(btn), 2e3);
+          setTimeout(function() {
+              $(btn).prop('disabled', false);
+          }.bind(btn), 1e4);
+        }
+        else
+        {
+          myModal2.innerHTML="Не заполнено обращение.";
+          modal.style.display = "block";
+          setTimeout(function() {
+            modal.style.display = "none";
+          }.bind(btn), 2e3);
+        }
+      }
+      else
+      {
+        modal.style.display = "block";
+        setTimeout(function() {
+          modal.style.display = "none";
+        }.bind(btn), 2e3);
+        myModal2.innerHTML="Не заполнены все данные.";
+      }
+    });
+    request.fail(function (jqXHR){
+      myModal2.innerHTML="Произошла ошибка.";
+    });
+  });
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ telephone.php
@@ -0,0 +1,26 @@
+<?php
+$telephone = filter_input(INPUT_POST,'telephone');
+$telephoneuser = filter_input(INPUT_POST,'telephoneUser');
+$TelephoneMessage = filter_input(INPUT_POST,'TelephoneMessage');
+$Date = new DateTime();
+$Date->setTimezone(new DateTimeZone('Asia/Yekaterinburg'));
+if (!empty($telephone and $TelephoneMessage)){
+    $serverName = "193.138.130.179, 16529";
+    $serverUser = "asis";
+    $serverPass = "OZ0i90";
+    if (empty($telephoneuser))
+    {
+        $telephoneuser="Anonymous";
+    }
+    $connectionInfo = array( "Database"=>"ASISSIte", "UID"=>$serverUser, "PWD"=>$serverPass, "CharacterSet" => "UTF-8");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    if( $conn ) {
+        $query = "INSERT Into Orders(CurrentDateTelephone,Telephone,TelephoneUser,TelephoneMessage) VALUES (?,?,?,?)";
+        $params = array($Date,$telephone,$telephoneuser,$TelephoneMessage);
+        $stmt = sqlsrv_query($conn,$query,$params);
+        }
+    else{
+        die('Something went wrong while connecting to MSSQL');
+        }
+}
+?>
